b258b0af71487781b75a51a224b0154daf5c34bb
blakeim2000 <41655447+blakeim2000@users.noreply.github.com>
2022-05-16 22:01:50 +1000

    Update nft.sol

--- code/nft.sol
+++ code/nft.sol
@@ -4,46 +4,34 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract SampleNFTContract is ERC721 {
     uint256 public tokenCounter;
-    
-    
+    uint256 public maxSupply = 3;
     //constructor for an ERC721 is a name and symbol
-    constructor () public ERC721 ("Next Chapter", "NEXT"){
+    constructor () public ERC721 ("Next Chapter", "NEXT") {
         tokenCounter = 0;
-        uint256 public constant maxSupply = 2;
     }
-    
+
     function _baseURI() internal view override returns (string memory) {
         return "https://github.com/blakeim2000/testNFT/tree/main/metadata/";
     }
-    
-    function tokenURI(uint256 _tokenId) public view returns (string) {
-    return Strings.strConcat(
-      baseTokenURI(),
-      Strings.uint2str(_tokenId),
-      ".json"
-     );
-    }
 
-    //a token url is a ipfs url
     //after we mint the token we are going to return the id of the token
     function createNFT() public returns (uint256) {
     
-    require (totalSupply() < maxSupply);
-    
-    //get number from token counter
+        require (tokenCounter < maxSupply);
+        
+        //get number from token counter
         uint256 newNFTTokenId = tokenCounter;
 
-    //safely mint token for the person that called the function
+        //safely mint token for the person that called the function
         _safeMint(msg.sender, newNFTTokenId);
-    
-    //set the token uri of the token id of the uri passed
-        tokenURI(newNFTTokenId, tokenURI(newNFTTokenId));
-    
-    //increment the counter
+        
+        //set the token uri of the token id of the uri passed
+        tokenURI(newNFTTokenId);
+
+        //increment the counter
         tokenCounter = tokenCounter + 1;
         
     //return the token id
         return newNFTTokenId;
     }
-
 }
