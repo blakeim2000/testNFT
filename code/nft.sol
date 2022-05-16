7e08aba9ba9268c333fe13711eac3341ca30d4f8
blakeim2000 <41655447+blakeim2000@users.noreply.github.com>
2022-05-16 20:15:02 +1000

    Update nft.sol

--- code/nft.sol
+++ code/nft.sol
@@ -19,7 +19,8 @@ contract SampleNFTContract is ERC721 {
     function tokenURI(uint256 _tokenId) public view returns (string) {
     return Strings.strConcat(
       baseTokenURI(),
-      Strings.uint2str(_tokenId)
+      Strings.uint2str(_tokenId),
+      ".json"
      );
     }
 
